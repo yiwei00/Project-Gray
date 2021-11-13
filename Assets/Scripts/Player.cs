0f905684852ffa94b3cc63282e6c9b98de2f0a16
Yi Wei <yiw00@outlook.com>
2021-11-12 21:13:33 -0500

    Implement rotation for player model

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,14 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, PlayerControls.IGameplayActions
 {
-    PlayerControls controls;
+    // public members
+    public float rotationSpeed = 5;
+    // use the method getMovementSpeed to get actual speed (need to factor in controller rates)
+    public float baseSpeed = 10;
 
-    Vector3 MovementDirection;
-    float velocity;
+    // private members
+    PlayerControls controls; // for getting control inputs
+    Vector3 MovementDirection; // set by player controls, normalized by default
 
     #region Input systems
     public void OnEnable()
@@ -31,14 +35,46 @@ public class Player : MonoBehaviour, PlayerControls.IGameplayActions
         MovementDirection = new Vector3(direction.x, 0.0f, direction.y);
     }
     #endregion
+
     void Start()
     {
-        velocity = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += velocity * MovementDirection * Time.deltaTime;
+        // updates movement and rotation
+        updateTransform();
+    }
+
+    // private methods:
+    private void updateTransform()
+    {
+        if (MovementDirection != Vector3.zero)
+        {
+            Quaternion rot = Quaternion.LookRotation(MovementDirection, Vector3.up);
+            // when rotating, have player move slower
+            if (transform.rotation == rot)
+            {
+                transform.position += baseSpeed * MovementDirection * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += .3f * baseSpeed * MovementDirection * Time.deltaTime;
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 100 * rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    /* * * * * * * * * * *
+     * 
+     * Public Methods Below:
+     * 
+     * * * * * * * * * * */
+    // Get actual movement speed of character, factoring in controller tilt
+    public float getMovementSpeed()
+    {
+        float controllerSpeed = MovementDirection.magnitude;
+        return controllerSpeed * baseSpeed;
     }
 }
